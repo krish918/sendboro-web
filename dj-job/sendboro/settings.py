570ba8b11fb50a28b1904ff986602941e187678d
Krishna Murti <s3_krish@yahoo.co.in>
2015-05-06 13:14:44 +0530

    added static root 3

--- dj-job/sendboro/settings.py
+++ dj-job/sendboro/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'html')
+STATIC_ROOT = BASE_DIR
 
 STATIC_URL = '/static/'
 
