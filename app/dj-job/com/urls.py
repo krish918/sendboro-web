25881fabb04a262ee07fbaa73e7df13521891989
Krishna Murti <s3_krish@yahoo.co.in>
2018-03-28 11:00:17 +0000

    added RawCall url

--- app/dj-job/com/urls.py
+++ app/dj-job/com/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url,patterns
-from com.views import AutoReplyInXml, CodeSpeechInXml, VoiceCallCode, RecordActionView
+from com.views import AutoReplyInXml, CodeSpeechInXml, VoiceCallCode, RecordActionView, RawCallView
 
 urlpatterns = [
                url(r'^com/api/reply$', AutoReplyInXml.as_view()),
                url(r'^com/api/voice/code/(?P<code>[0-9]{5,5})$', CodeSpeechInXml.as_view()),
                url(r'^com/api/voicecall$', VoiceCallCode.as_view()),
                url(r'^com/api/record$', RecordActionView.as_view()),
+               url(r'^com/api/rawcall$', RawCallView.as_view()),
             ]
